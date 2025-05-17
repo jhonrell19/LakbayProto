78fce478b3e1024b42d00f662f0be512c3eed363
Owner <jhonrellgestupa4@gmai.com>
2025-05-17 22:33:17 +0800

    Initial Commit

---
+++ app/src/main/java/com/prot/lakbayapp/LoginActivity.kt
@@ -0,0 +1,36 @@
+package com.prot.lakbayapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.prot.lakbayapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                val intent = Intent(this, RouteSelectionActivity::class.java) // Correct Activity name
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.registerButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java) // Correct Activity name
+            startActivity(intent)
+        }
+    }
+}
